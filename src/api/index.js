3d9b39a608f8bf2e4381c8dd378af4d6ebc9aed0
Christoffer Artmann <artgaard@gmail.com>
2022-09-08 15:28:01 +0200

    feat: Create the different routes.

---
+++ src/api/index.js
@@ -0,0 +1,34 @@
+class Api {
+  async fetchTestSuites() {
+    const testSuites = await this._get('/test_suites')
+
+    return testSuites
+  }
+
+  async fetchTestSuite(id) {
+
+  }
+
+  async _get(path) {
+    return this._request(path, 'GET')
+  }
+
+  async _request(path, method) {
+    const host = process.env['API_URL'] ?? 'http://localhost:3456'
+    const url = `${host}${path}`
+
+    const response = await fetch(url, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method
+    })
+
+    const data = await response.json()
+
+    return data
+  }
+}
+
+export const api = new Api()
